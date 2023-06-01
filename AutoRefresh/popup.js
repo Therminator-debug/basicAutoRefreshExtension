ab678c26b219f47a48fc53a32dc7eb55b2c0a331
Therminator-debug <82982540+Therminator-debug@users.noreply.github.com>
2023-05-31 20:06:53 -0500

    Add files via upload

---
+++ AutoRefresh/popup.js
@@ -0,0 +1,25 @@
+let intervalId = null;
+
+function startRefresh() {
+  const seconds = parseInt(document.getElementById('secInput').value) || 0;
+  const minutes = parseInt(document.getElementById('minInput').value) || 0;
+  const interval = (seconds + (minutes * 60)) * 1000;
+
+  if (intervalId !== null) {
+    clearInterval(intervalId);
+  }
+
+  intervalId = setInterval(() => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      chrome.tabs.reload(tabs[0].id);
+    });
+  }, interval);
+}
+
+function stopRefresh() {
+  clearInterval(intervalId);
+  intervalId = null;
+}
+
+document.getElementById('buttonStart').addEventListener('click', startRefresh);
+document.getElementById('buttonStop').addEventListener('click', stopRefresh);
