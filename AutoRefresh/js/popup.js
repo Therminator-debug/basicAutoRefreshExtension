5d0686fdda86765f0610135edc316e9a60950460
Therminator-debug <82982540+Therminator-debug@users.noreply.github.com>
2023-06-10 14:01:31 -0500

    Update popup.js

--- AutoRefresh/js/popup.js
+++ AutoRefresh/js/popup.js
@@ -1,25 +1,27 @@
-let intervalId = null;
+document.addEventListener('DOMContentLoaded', function() {
+  let intervalId = null;
 
-function startRefresh() {
-  const seconds = parseInt(document.getElementById('secInput').value) || 0;
-  const minutes = parseInt(document.getElementById('minInput').value) || 0;
-  const interval = (seconds + (minutes * 60)) * 1000;
+  function startRefresh() {
+    const seconds = parseInt(document.getElementById('secInput').value) || 0;
+    const minutes = parseInt(document.getElementById('minInput').value) || 0;
+    const interval = (seconds + (minutes * 60)) * 1000;
 
-  if (intervalId !== null) {
-    clearInterval(intervalId);
+    if (intervalId !== null) {
+      clearInterval(intervalId);
+    }
+
+    intervalId = setInterval(() => {
+      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        chrome.tabs.reload(tabs[0].id);
+      });
+    }, interval);
   }
 
-  intervalId = setInterval(() => {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      chrome.tabs.reload(tabs[0].id);
-    });
-  }, interval);
-}
+  function stopRefresh() {
+    clearInterval(intervalId);
+    intervalId = null;
+  }
 
-function stopRefresh() {
-  clearInterval(intervalId);
-  intervalId = null;
-}
-
-document.getElementById('Start').addEventListener('click', startRefresh);
-document.getElementById('Stop').addEventListener('click', stopRefresh);
+  document.getElementById('Start').addEventListener('click', startRefresh);
+  document.getElementById('Stop').addEventListener('click', stopRefresh);
+});
